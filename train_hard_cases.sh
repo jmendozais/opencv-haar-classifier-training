6973b095fcbb7829e61c354e5b39f768f8f46488
Julio Mendoza <jmendozais@gmail.com>
2015-07-15 20:35:27 -0300

    Fix max z-angle param

--- train_hard_cases.sh
+++ train_hard_cases.sh
@@ -1,7 +1,7 @@
 # create positive patches in vec file
 perl bin/createsamples.pl hard_cases_positives.txt negatives.txt hc_samples 5000\
   "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1\
-  -maxyangle 1.1 -maxzangle 1.1 -maxidev 40 -w 54 -h 40"
+  -maxyangle 1.1 -maxzangle 0.25 -maxidev 40 -w 54 -h 40"
 
 # create positive patches in format
 # perl bin/createsamples.pl positives.txt negatives.txt samples 1500 "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 -maxzangle 0.1 -maxidev 40 -w 54 -h 40 -info samples/generated.txt"
